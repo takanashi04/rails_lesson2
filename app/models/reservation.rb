5a906d7a34b1bbc683a1e180fbcf0fd91a6f550d
Tsuyoshi Takanashi <takenashi@me.com>
2023-06-30 12:57:09 +0900

    add:rooms,reservationsのDB統合

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,4 +2,6 @@ class Reservation < ApplicationRecord
   validates :check_in_date, presence: true
   validates :check_out_date, presence: true
   validates :number_of_person, presence: true
+
+  belongs_to :room
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -5,4 +5,6 @@ class Room < ApplicationRecord
   validates :address, presence: true
 
   mount_uploader :avatar_path, AvatarUploader
+
+  has_many :reservations
 end
