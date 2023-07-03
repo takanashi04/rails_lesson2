531954e49755d8a557a0edbb553137042513a38d
Tsuyoshi Takanashi <takenashi@me.com>
2023-07-03 13:32:02 +0900

    fix:roomsのビューを修正、routes変更

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'home#index'
   devise_for :users
   post '/rooms/new', to:'rooms#create'
+  post '/rooms/:id', to:'reservations#create'
   resources :rooms
   resources :reservations
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
